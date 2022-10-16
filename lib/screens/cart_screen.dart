f050b4afcb8957c1333e1434fd379dd50099109e
Ahmed Maher Halima <phpcodertop@gmail.com>
2022-10-16 02:50:07 +0200

    Changes

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,10 +3,8 @@ import 'package:food_delivery/data/data.dart';
 import 'package:food_delivery/models/order.dart';
 
 class CartScreen extends StatefulWidget {
-  const CartScreen({Key? key}) : super(key: key);
-
   @override
-  State<CartScreen> createState() => _CartScreenState();
+  _CartScreenState createState() => _CartScreenState();
 }
 
 class _CartScreenState extends State<CartScreen> {
@@ -15,10 +13,10 @@ class _CartScreenState extends State<CartScreen> {
       padding: const EdgeInsets.all(20.0),
       height: 170.0,
       child: Row(
-        children: [
+        children: <Widget>[
           Expanded(
             child: Row(
-              children: [
+              children: <Widget>[
                 Container(
                   width: 150.0,
                   decoration: BoxDecoration(
@@ -35,73 +33,65 @@ class _CartScreenState extends State<CartScreen> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
+                      children: <Widget>[
                         Text(
                           order.food.name,
                           style: const TextStyle(
-                            fontWeight: FontWeight.bold,
                             fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
                           ),
                           overflow: TextOverflow.ellipsis,
                         ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
+                        const SizedBox(height: 10.0),
                         Text(
                           order.restaurant.name,
                           style: const TextStyle(
-                            fontWeight: FontWeight.w600,
                             fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
                           ),
                           overflow: TextOverflow.ellipsis,
                         ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
+                        const SizedBox(height: 10.0),
                         Container(
                           width: 100.0,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10.0),
                             border: Border.all(
                               width: 0.8,
-                              color: Colors.black45,
+                              color: Colors.black54,
                             ),
                           ),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
+                            children: <Widget>[
                               GestureDetector(
                                 onTap: () {},
-                                child: const Text(
+                                child: Text(
                                   '-',
                                   style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
                                     fontSize: 20.0,
                                     fontWeight: FontWeight.w600,
-                                    color: Colors.deepOrangeAccent,
                                   ),
                                 ),
                               ),
-                              const SizedBox(
-                                width: 20.0,
-                              ),
+                              const SizedBox(width: 20.0),
                               Text(
                                 order.quantity.toString(),
                                 style: const TextStyle(
-                                  fontSize: 24.0,
-                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600,
                                 ),
                               ),
-                              const SizedBox(
-                                width: 20.0,
-                              ),
+                              const SizedBox(width: 20.0),
                               GestureDetector(
                                 onTap: () {},
-                                child: const Text(
+                                child: Text(
                                   '+',
                                   style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
                                     fontSize: 20.0,
                                     fontWeight: FontWeight.w600,
-                                    color: Colors.deepOrangeAccent,
                                   ),
                                 ),
                               ),
@@ -111,15 +101,18 @@ class _CartScreenState extends State<CartScreen> {
                       ],
                     ),
                   ),
-                ),
+                )
               ],
             ),
           ),
-          Text(
-            '\$${order.quantity * order.food.price}',
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: Text(
+              '\$${order.quantity * order.food.price}',
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
             ),
           ),
         ],
@@ -129,22 +122,105 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+          (Order order) => totalPrice += order.quantity * order.food.price,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart (${currentUser.cart.length})'),
       ),
       body: ListView.separated(
-        itemBuilder: (BuildContext content, int index) {
-          Order order = currentUser.cart[index];
-          return _buildCartItem(order);
-        },
-        separatorBuilder: (BuildContext content, int index) {
-          return const Divider(
-            color: Colors.grey,
-            height: 1.0,
+        itemCount: currentUser.cart.length + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index < currentUser.cart.length) {
+            Order order = currentUser.cart[index];
+            return _buildCartItem(order);
+          }
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const <Widget>[
+                    Text(
+                      'Estimated Delivery Time:',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '25 min',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const Text(
+                      'Total Cost:',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '\$${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.green[700],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 80.0),
+              ],
+            ),
           );
         },
-        itemCount: currentUser.cart.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider(
+            height: 1.0,
+            color: Colors.grey,
+          );
+        },
+      ),
+      bottomSheet: Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, -1),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Center(
+          child: ElevatedButton(
+            child: const Text(
+              'CHECKOUT',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ),
       ),
     );
   }
